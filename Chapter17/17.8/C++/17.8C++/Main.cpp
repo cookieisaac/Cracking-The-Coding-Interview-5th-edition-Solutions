aa56896050164360c3288f1622a3b45a8b3de810
cookieisaac <cookieisaac.zju@gmail.com>
2014-09-04 19:12:21 -0400

    17.8 Update Solution

--- Chapter17/17.8/C++/17.8C++/Main.cpp
+++ Chapter17/17.8/C++/17.8C++/Main.cpp
@@ -23,7 +23,37 @@ int main(void)
 
 	while (!inputFile.eof())
 	{
-		//Implement Testing Here
+		int N;
+		inputFile >> N;
+		int * array = (int*)malloc(N* sizeof(int));
+
+		for (int i = 0; i < N; i++)
+		{
+			inputFile >> array[i];
+		}
+
+		outputFile << "Array of size " << N << " : ( ";
+		for (int i = 0; i < N; i++)
+		{
+			if (i == N - 1)
+				outputFile << array[i] << " ) " << endl;
+			else
+				outputFile << array[i] << ", ";
+		}
+
+		int start = 0;
+		int end = 0;
+		outputFile << "Max Sum is " << findMaxSum(array, N, start, end);
+		outputFile << " from range " << start << " to " << end << ": ( ";
+		for (int i = start; i <= end; i++)
+		{
+			if (i == end)
+				outputFile << array[i] << " ) " << endl;
+			else
+				outputFile << array[i] << ", ";
+		}
+		outputFile << "************************************************************************************************" << endl;
+
 	}
 
 	inputFile.close();
