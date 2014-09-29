da4f82732bef2f543ab3aa72f973a4eed7498436
cookieisaac <cookieisaac.zju@gmail.com>
2014-09-29 18:47:27 -0400

    Add 01.02 solution

---
+++ Chapter01/01.02/CPP/01.02C++/Main.cpp
@@ -0,0 +1,45 @@
+#include <fstream>
+#include <iostream>
+#include "Solution.h"
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+	ifstream inputFile("Input.txt");
+	ofstream outputFile("Output.txt");
+
+	if (!inputFile)
+	{
+		cout << "Can't open Input.txt!" << endl;
+		exit (1);
+	}
+
+	if (!outputFile)
+	{
+		cout << "Can't open Output.txt!" << endl;
+		exit (1);
+	}
+
+	while (!inputFile.eof())
+	{
+		//1. Get CPP string
+		string line;
+		getline(inputFile, line);
+		//2. Convert to NULL ended string. Note c_str returns const char*, so we have to make a copy of it.
+		char *cstr = (char*)malloc(sizeof(char)*(line.length() + 1));
+		strcpy_s(cstr, line.length()+1, line.c_str());
+		//3. reverse the string
+		reverse(cstr);
+		//4. Create a reversed string
+		string reversed_line = string(cstr);
+
+		outputFile << "Reverse of string \"" << line << "\" is: \""<< reversed_line<< "\""<< endl;
+	}
+
+	inputFile.close();
+	outputFile.close();
+
+	return 0;
+}
