722e8d20f16c3b5d0bf1973468c12f6c591288f1
cookieisaac <cookieisaac.zju@gmail.com>
2014-10-09 16:07:40 -0400

    Add 02.03CPP solution

--- Chapter02/02.03/CPP/02.03C++/Solution.cpp
+++ Chapter02/02.03/CPP/02.03C++/Solution.cpp
@@ -1,1 +1,43 @@
 #include "Solution.h"
+using namespace std;
+
+void LinkedList::addNodeAtHead(Node* pNode)
+{
+	if (m_head == NULL)
+	{
+		m_head = pNode;
+		m_size++;
+	}
+	else
+	{
+		pNode->setNext(m_head);
+		m_head = pNode;
+		m_size++;
+	}
+}
+
+void LinkedList::deleteByNode(Node* pNode)
+{
+	// Limitation of this method is that we can't delete the last node
+	if (pNode->getNext() == NULL)
+	{
+		return; 
+	}
+	pNode->setValue(pNode->getNext()->getValue());
+	pNode->setNext(pNode->getNext()->getNext());
+	m_size--;
+}
+
+std::ostream& operator << (std::ostream & os, LinkedList& ll)
+{
+	Node* pNode = ll.getHead();
+
+	while (pNode != NULL)
+	{
+		os << pNode->getValue() << " -> ";
+		pNode = pNode->getNext();
+	}
+	os << "NULL" << endl;
+
+	return os;
+}
--- Chapter02/02.03/CPP/02.03C++/Solution.h
+++ Chapter02/02.03/CPP/02.03C++/Solution.h
@@ -1,6 +1,37 @@
 #ifndef _SOLUTION_H
 #define _SOLUTION_H
 
+#include <ostream>
+
+class Node;
+
+class LinkedList
+{
+public:
+	LinkedList(){ m_head = NULL; m_size = 0; }
+	Node* getHead(){ return m_head; }
+	int getSize() { return m_size; }
+	void addNodeAtHead(Node* pNode);
+	void deleteByNode(Node* pNode);
+	friend std::ostream& operator << (std::ostream & os, LinkedList&);
+private:
+	Node* m_head;
+	int m_size;
+};
+
+class Node
+{
+public:
+	Node(int value=0) :m_value(value){ m_next = NULL; }
+	void setValue(int value){ m_value = value; }
+	void setNext(Node* pNode) { m_next = pNode; }
+	int getValue(){ return m_value; }
+	Node* getNext(){ return m_next; }
+	~Node(){ m_next = NULL; }
+private:
+	int m_value;
+	Node* m_next;
+};
 
 
 #endif
