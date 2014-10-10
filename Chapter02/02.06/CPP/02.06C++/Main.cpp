433b9fc32cd62af585d13e892c3b2dbebe248648
cookieisaac <cookieisaac.zju@gmail.com>
2014-10-10 16:58:16 -0400

    Add 02.06 C++ solution

--- Chapter02/02.06/CPP/02.06C++/Main.cpp
+++ Chapter02/02.06/CPP/02.06C++/Main.cpp
@@ -1,5 +1,7 @@
 #include <fstream>
 #include <iostream>
+#include <string>
+#include <sstream>
 #include "Solution.h"
 
 using namespace std;
@@ -23,7 +25,34 @@ int main(void)
 
 	while (!inputFile.eof())
 	{
-		//Implement Testing Here
+		string list;
+		getline(inputFile, list);
+		stringstream s1;
+		s1 << list;
+
+		string entry;
+		getline(inputFile, entry);
+		stringstream s2;
+		s2 << entry;
+
+		char loopEntry;
+		s2 >> loopEntry;
+
+		CircularList circularList;
+		s1 >> circularList;
+		outputFile << "The original list is: " << circularList << " of size " << circularList.getSize() << endl;
+		circularList.createCircle(loopEntry);
+		outputFile << "After creating loop at '" << loopEntry << "', the list is: " << circularList << endl;
+		Node* loopPoint = circularList.findLoopEntry();
+		if (loopPoint != NULL)
+		{
+			outputFile << "Function findLoopEntry() finds out the loop entry is at '" << loopPoint->getValue() << "'." << endl;
+		}
+		else
+		{
+			outputFile << "Function findLoopEntry() couldn't find a loop entry." << endl;
+		}
+		outputFile << "****************************************************************************************************************" << endl;
 	}
 
 	inputFile.close();
--- Chapter02/02.06/CPP/02.06C++/Solution.cpp
+++ Chapter02/02.06/CPP/02.06C++/Solution.cpp
@@ -1,1 +1,147 @@
 #include "Solution.h"
+
+std::istream& operator>>(std::istream& is, CircularList& cl)
+{
+	char value;
+
+	while (is >> value)
+	{
+		cl.addNodeAtTail(value);
+	}
+
+	return is;
+}
+std::ostream& operator<<(std::ostream& os, CircularList& cl)
+{
+	Node* runner = cl.getHead();
+
+	if (runner == NULL)
+	{
+		os << "( NULL )";
+		return os;
+	}
+
+	int counter = 0;
+	int loopGuard = 3 * cl.getSize();
+	os << "( ";
+	while (runner->getNext() != NULL && counter < loopGuard)
+	{
+		counter++;
+		os << runner->getValue() << " --> ";
+		runner = runner->getNext();
+	}
+
+	if (runner->getNext() == NULL)
+	{
+		os << runner->getValue() << " -> NULL )";
+	}
+	else
+	{
+		os << runner->getValue() << " -> Infinity.... )";
+	}
+
+	return os;
+}
+
+CircularList::~CircularList()
+{
+	int counter = m_size;
+	Node* runner = m_head;
+	while (counter > 0)
+	{
+		m_head = runner->getNext();
+		free(runner);
+		runner = m_head;
+		counter--;
+	}
+}
+
+void CircularList::addNodeAtTail(char c)
+{
+	Node* pNode = (Node*)malloc(sizeof(Node));
+	pNode->setNextNULL();
+	pNode->setValue(c);
+
+	m_size++;
+
+	if (m_head == NULL)
+	{
+		m_head = pNode;
+	}
+	else
+	{
+		Node* runner = m_head;
+		while (runner->getNext() != NULL)
+		{
+			runner = runner->getNext();
+		}
+		runner->setNext(pNode);
+	}
+}
+
+void CircularList::createCircle(char c)
+{
+	Node* runner = m_head;
+	Node* finder = NULL;
+
+	if (runner == NULL)
+	{
+		return;
+	}
+
+	while (runner->getNext() != NULL)
+	{
+		if (runner->getValue() == c)
+		{
+			finder = runner;
+		}
+
+		runner = runner->getNext();
+	}
+
+	if (runner->getValue() == c)
+	{
+		finder = runner;
+	}
+
+	runner->setNext(finder);
+
+	return;
+}
+
+
+Node* CircularList::findLoopEntry()
+{
+	Node* result = NULL;
+	Node* collision = NULL;
+	Node* fastRunner = m_head;
+	Node* slowRunner = m_head;
+
+	while (fastRunner != NULL&& fastRunner->getNext() != NULL)
+	{
+		fastRunner = fastRunner->getNext()->getNext();
+		slowRunner = slowRunner->getNext();
+
+		if (fastRunner == slowRunner)
+		{
+			collision = slowRunner;
+			break;
+		}
+	}
+
+	if (collision != NULL)
+	{
+		Node* slowRunner2 = m_head;
+		slowRunner = collision;
+
+		while (slowRunner != slowRunner2)
+		{
+			slowRunner = slowRunner->getNext();
+			slowRunner2 = slowRunner2->getNext();
+		}
+
+		result = slowRunner;
+	}
+
+	return result;
+}
