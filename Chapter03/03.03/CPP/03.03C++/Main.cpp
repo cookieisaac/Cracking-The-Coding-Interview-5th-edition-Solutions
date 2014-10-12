6dd2d4e5fdff0982604ff4b869031155470bbee9
cookieisaac <cookieisaac.zju@gmail.com>
2014-10-11 20:09:44 -0400

    Add 03.03 c++ solution

--- Chapter03/03.03/CPP/03.03C++/Main.cpp
+++ Chapter03/03.03/CPP/03.03C++/Main.cpp
@@ -21,10 +21,60 @@ int main(void)
 		exit (1);
 	}
 
-	while (!inputFile.eof())
+	SetOfStacks mySet;
+
+
+	for (int value = 0; value < 100; value++)
 	{
-		//Implement Testing Here
+		mySet.push(value);
 	}
+
+	outputFile << "Initial set of stacks: " << endl;
+	mySet.printState(outputFile) ;
+	outputFile << "********************************************************************************" << endl;
+	
+	for (int stack = 0; stack < mySet.getNumStacks(); stack++)
+	{
+		for (int popTimes = stack; popTimes > 0; popTimes--)
+		{
+			mySet.popAt(stack);
+		}
+	}
+
+	outputFile << "After popping n elements at stack n: " << endl;
+	mySet.printState(outputFile);
+	outputFile << "********************************************************************************" << endl;
+
+	for (int counter = 0; counter < 7; counter++)
+	{
+		mySet.pop();
+	}
+
+	outputFile << "After popping 7 elements at set of stacks: " << endl;
+	mySet.printState(outputFile);
+	outputFile << "********************************************************************************" << endl;
+
+	for (int counter = 0; counter < 7; counter++)
+	{
+		mySet.popAt(2);
+	}
+
+	outputFile << "After popping 7 elements at stack[2]: " << endl;
+	mySet.printState(outputFile);
+	outputFile << "********************************************************************************" << endl;
+
+	mySet.popAt(2);
+	outputFile << "After popping one more time at stack[2]: " << endl;
+	mySet.printState(outputFile);
+	outputFile << "********************************************************************************" << endl;
+
+	while (!mySet.isEmpty())
+	{
+		mySet.pop();
+	}
+	outputFile << "After popping all elements: " << endl;
+	mySet.printState(outputFile);
+	outputFile << "********************************************************************************" << endl;
 
 	inputFile.close();
 	outputFile.close();
